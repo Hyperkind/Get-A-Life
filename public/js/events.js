04d81879ef4753235bf55be68f56ea951888a006
Jon Ah-Nee <jonahnee@gmail.com>
2016-03-26 19:32:34 -1000

    working the non-angular way

--- public/js/events.js
+++ public/js/events.js
@@ -1,13 +1,13 @@
 var lat = '21.3096086419986';
-var long = '-157.8086566532421';
+var lng = '-157.8086566532421';
 
-tktMstrAPI(lat,long);
-evntBriteAPI(lat, long);
+tktMstrAPI(lat,lng);
+evntBriteAPI(lat, lng);
 
-function tktMstrAPI(lat, long) {
+function tktMstrAPI(lat, lng) {
   $.ajax({
     method: "GET",
-    url: "https://app.ticketmaster.com/discovery/v1/events.json?latlong=" + lat + "%2C" + long + "&apikey=kejQVntR3erc03OmcE6rGwHIlVV0aNG0",
+    url: "https://app.ticketmaster.com/discovery/v1/events.json?latlong=" + lat + "%2C" + lng + "&apikey=kejQVntR3erc03OmcE6rGwHIlVV0aNG0",
     async: true,
     dataType: "json",
     success: function(data) {
@@ -19,10 +19,10 @@ function tktMstrAPI(lat, long) {
   });
 }
 
-function evntBriteAPI(lat, long) {
+function evntBriteAPI(lat, lng) {
   $.ajax({
     method: "GET",
-    url: "https://www.eventbriteapi.com/v3/events/search/?location.latitude=" + lat + "&location.longitude=" + long + "&token=BDMUJMKAWOGCYVJD7SYA",
+    url: "https://www.eventbriteapi.com/v3/events/search/?location.latitude=" + lat + "&location.longitude=" + lng + "&token=BDMUJMKAWOGCYVJD7SYA",
     async: true,
     dataType: "json",
     success: function(data) {
