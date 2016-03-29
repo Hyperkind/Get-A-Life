679ed7630bc5b7fff0128cfc9a0fed1e5890b6de
Jon Ah-Nee <jonahnee@gmail.com>
2016-03-28 20:29:48 -1000

    fixed server file

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 var morgan = require('morgan');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
-var CONFIG = require('../config.json');
 
 var app = express();
 
