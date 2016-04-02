7d139879ff0a9f22bc2d5d832269ff5e0ae0eb0e
benjaminpettus <benjaminpettus@gmail.com>
2016-04-02 12:50:51 -1000

    marker at current location

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ui-leaflet'])
+angular.module('myApp', ['ionic', 'starter.controllers', 'ui-leaflet'])
  
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -64,7 +64,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui-leaflet'])
     views: {
       'menuContent': {
         templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+        controller: 'PlaylistsCtrl'
       }
     }
   });
