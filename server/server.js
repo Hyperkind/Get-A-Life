0593daa4c5f10f7ba039a3145da50e4c809ba80b
Gail Matsushima <gail.matsushima@gmail.com>
2016-04-07 12:58:17 -0700

    revised CORS cross-origin on server.js can now edit and delete events

--- server/server.js
+++ server/server.js
@@ -52,7 +52,10 @@ app.use(passport.session());
 //CORS cross origin between server (localhost:3000 and 8100)
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  // res.header("Access-Control-Allow-Headers", "*", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
